7ebd49f0abea9b27e851c3071b448e7cdb5590dd
Catherine Baker <90419216+cbaker02@users.noreply.github.com>
2024-02-06 18:28:03 +0000

    Array of instructions from txt works

--- Project1.java
+++ Project1.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 import java.lang.Runtime;
 
 public class Project1 {
@@ -11,33 +13,67 @@ public class Project1 {
             String filename = args[0];
             Runtime rt = Runtime.getRuntime();
 
-            File fileName = new File(args[0]);
+            // String filename = "example1.txt";
+            // File fileName = new File(args[0]);
 
-            System.out.println("Is this the file you want to open? " + fileName);
+            // System.out.println("Is this the file you want to open? " + filename);
 
-            fileManager(filename);
+            List<Integer> instr = fileManager(filename);
 
-            System.out.println("You made it back to main!!");
+            // System.out.println("You made it back to main!!");
+
+            for(int i = 0; i < instr.size(); i++){
+                System.out.println(instr.get(i));
+            }
+
 
         } catch (Throwable t) {
             t.printStackTrace();
         }
     }
     
-    public static void fileManager(String fileName) {
+    public static List<Integer> fileManager(String fileName) {
+        // This functions opens and parses the given file 
         try {
+            // Setup to parse the file
             File file = new File(fileName);
-            Scanner in = new Scanner(file);
+            Scanner in = new Scanner(file); 
+            List<Integer> instruction = new ArrayList<Integer>();
 
             while (in.hasNextLine()) {
+                // Parse by line and split by space
                 String line = in.nextLine();
-                System.out.println(line);
-            }
+                String[] words = line.split(" ");
 
+                int curIns = -1;
+                
+                // Send only the instruction to verify int
+                curIns = getInt(words[0]);
+
+                // Verifies that it's not the initialized var 
+                // and adds to the array of instructions
+                if(curIns != -1) {
+                    instruction.add(curIns);
+                }
+            }
             in.close();
+            return instruction;
+
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+        return null;
+    }
+
+    public static int getInt(String curr) {
+        // Verify that the first item was an int and pass back
+        int curIns = -1;
+        try {
+            curIns = Integer.parseInt(curr);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return curIns;
     }
 }
